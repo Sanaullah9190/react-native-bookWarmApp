59aaf205dbeeb497b6f52b05100b0f257396d700
sanaullah ansari <sanaullaht.social@gmail.com>
2026-04-10 23:02:18 +0530

    backend changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,9 +11,9 @@ const PORT = process.env.PORTS || 3000
 
 
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cors())
-app.use(express.urlencoded({ extended: true, limit: "20kb" }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
